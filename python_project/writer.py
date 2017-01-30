1a75dbf60f9c5d29f8bb43c9c1a118a4d8f5a63b
Trevor Kirsch <tjkirsch@iastate.edu>
2017-01-29 20:10:58 -0600

    Added writer.py and streamdata.xml.

---
+++ python_project/writer.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+import urllib2
+import xml.etree.ElementTree as ET
+
+tree=ET.parse('stream.xml')
+root=tree.getroot()
+
+f=open('streamdata.xml','w')
+
+for i in range(1,100,1):
+	z=root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()[i]
+	f.write(z.getchildren()[0].text)
+	f.write(z.getchildren()[1].text)
+
+f.close()
+
