a9e97a217748df0e771800dbc462cd81d4a63fa1
Trevor Kirsch <tjkirsch@iastate.edu>
2017-01-30 16:59:01 -0600

    Updated writer.py to include formatting.

--- python_project/writer.py
+++ python_project/writer.py
@@ -12,7 +12,9 @@ f=open('streamdata.xml','w')
 for i in range(1,100,1):
 	z=root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()[i]
 	f.write(z.getchildren()[0].text)
+	f.write('\n')
 	f.write(z.getchildren()[1].text)
+	f.write('\n')
 
 f.close()
 
